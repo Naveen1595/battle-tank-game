75f7d5afd3cb2ae2f052050183be7d1995991563
Naveen1595 <naveen.rtw1595@gmail.com>
2020-11-03 14:05:28 +0530

    MVC Script added

---
+++ Assets/Scripts/MVC/Tank/TankController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoSingletonGeneric<TankController>
+{
+    public TankController(TankModel tankModel, TankView tankView)
+    {
+        TankNewModel = tankModel;
+        TankNewView = GameObject.Instantiate<TankView>(tankView);
+    }
+
+    public TankModel TankNewModel { get; private set; }
+    public TankView TankNewView { get; private set; }
+
+}
---
+++ Assets/Scripts/MVC/Tank/TankService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : MonoBehaviour
+{
+    [SerializeField] TankView tankView;
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        CreateTank();
+    }
+
+    private TankController CreateTank()
+    {
+        TankModel tankModel = new TankModel(5, 40f);
+        TankController tank = new TankController(tankModel, tankView);
+        return tank;
+    }
+}
