121caf21168c39a67d66d9150d78240b68d86793
Naveen1595 <naveen.rtw1595@gmail.com>
2020-12-18 22:40:37 +0530

    camera transition added

--- Assets/Scripts/Game/CameraMovementController.cs
+++ Assets/Scripts/Game/CameraMovementController.cs
@@ -6,10 +6,17 @@ public class CameraMovementController : MonoBehaviour
 {
     [SerializeField] private Transform playerTankTransform;
     Camera cam;
+    float transitionSpeed = 2;
+    Transform nextLocation;
+    [SerializeField] DestroyAll destroyAll;
+    
+    bool isPlayerAlive = true;
 
     private void Awake()
     {
         cam = GetComponentInChildren<Camera>();
+        //destroyAll = GetComponent<DestroyAll>();
+
     }
 
     private void FixedUpdate()
@@ -19,8 +26,22 @@ public class CameraMovementController : MonoBehaviour
 
     private void CameraPosition()
     {
-        cam.orthographicSize = 20f;
-        gameObject.transform.position = playerTankTransform.position;
+        isPlayerAlive = destroyAll.GetPlayerStatus();
+        if (isPlayerAlive)
+        {
+            cam.orthographicSize = 20f;
+            transform.position = playerTankTransform.position;
+        }
+        else if(!destroyAll.GetGameStatus() && !isPlayerAlive)
+        {
+            nextLocation = destroyAll.GetEnemyTankTransform();
+            transform.position = Vector3.Lerp(transform.position, nextLocation.position, Time.deltaTime * transitionSpeed);
+        }
+        else
+        {
+            transform.position = new Vector3(0f, 0f, 0f);
+        }
+        
     }
 
 }
--- Assets/Scripts/MVC/Tank/DestroyAll.cs
+++ Assets/Scripts/MVC/Tank/DestroyAll.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class DestroyAll : MonoBehaviour
@@ -6,19 +7,26 @@ public class DestroyAll : MonoBehaviour
     List<GameObject> EnemyTankGameObject = new List<GameObject>();
     int noOfEnemyTank;
     float secToWait = 3f;
+    bool isPlayerAlive = true;
+    bool isGameOver = false;
     EnemySpawnerService enemySpawnerService;
+    Transform currentEnemyTransform;
+    ActiveEnemyController activeEnemyController;
+
 
 
     void Awake()
     {
+        isPlayerAlive = true;
         enemySpawnerService = GameObject.FindObjectOfType<EnemySpawnerService>();
         noOfEnemyTank = enemySpawnerService.GetNoOfEnemy();
         EnemyTankGameObject = enemySpawnerService.GetEnemy();
         TankController.Instance().PlayerDeath += DestroyAllObject;
-       
+        
     }
     private void DestroyAllObject()
     {
+        isPlayerAlive = false;
         TankController.Instance().gameObject.SetActive(false);
         StartCoroutine(DestroyingStarted());
     }
@@ -27,9 +35,30 @@ public class DestroyAll : MonoBehaviour
     {
        for(int i= 0;i< noOfEnemyTank; i++)
        {
-            yield return new WaitForSeconds(secToWait);
-            Destroy(EnemyTankGameObject[i]);
-            
+            if(EnemyTankGameObject[i] != null)
+            {
+                activeEnemyController = EnemyTankGameObject[i].GetComponent<ActiveEnemyController>();
+                currentEnemyTransform = EnemyTankGameObject[i].transform;
+                yield return new WaitForSeconds(secToWait);
+                activeEnemyController.OnDeath();
+            }
+   
        }
+        isGameOver = true;
+    }
+
+    public bool GetPlayerStatus()
+    {
+        return isPlayerAlive;
+    }
+
+    public bool GetGameStatus()
+    {
+        return isGameOver;
+    }
+
+    public Transform GetEnemyTankTransform()
+    {
+        return currentEnemyTransform;
     }
 }
